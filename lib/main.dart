baf0432a386a2dfda361517d1c8d8ca777163f50
Lucas Garcia Mendes <lucasgarciamendes1997@hotmail.com>
2025-10-15 22:08:07 -0300

    feat: Add internationalization support to main application

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:maestro_test/core/design_system/app_theme.dart';
 import 'package:maestro_test/di/service_locator.dart';
 import 'package:maestro_test/features/todo/views/todo_view.dart';
+import 'package:maestro_test/l10n/app_localizations.dart';
 
 void main() {
   setupDI();
@@ -16,9 +17,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo List',
       debugShowCheckedModeBanner: false,
+
+      // Internationalization configuration
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+
+      // Theme configuration
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.light,
+
       home: const TodoView(),
     );
   }
